92e655cd4289100aedd68348bba79d2773689c95
YAGI0423 <9945735@naver.com>
2022-01-02 23:49:15 +0900

    connect `home`, `lobby` model with page

--- omok_server/lobby/views.py
+++ omok_server/lobby/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import LiveRoom
+
+def index(request):
+    liveRooms = LiveRoom.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'lobby/index.html',
+        {
+            'liveRooms': liveRooms,
+        }
+    )
