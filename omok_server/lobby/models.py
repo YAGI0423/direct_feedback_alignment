6e1b2f12c89165ce6729e40c31621ff8a6dc0f65
YAGI0423 <9945735@naver.com>
2022-01-02 21:50:36 +0900

    create `lobby model` and set `timezone`

--- omok_server/lobby/models.py
+++ omok_server/lobby/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class LiveRoom(models.Model):
+    roomName = models.CharField(max_length=30)
+    hostName = models.CharField(max_length=10)
+    gameStatus = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'[{self.pk}] {self.roomName} ({self.hostName})'
--- omok_server/omok_server/settings.py
+++ omok_server/omok_server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'lobby',
 ]
 
 MIDDLEWARE = [
@@ -105,13 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'   #default=UTC
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False #defalut=True
 
 
 # Static files (CSS, JavaScript, Images)
