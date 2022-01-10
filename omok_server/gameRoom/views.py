3e8e00baabbdb25a0853287856bbfdbee6fd9225
YAGI0423 <9945735@naver.com>
2022-01-10 16:20:54 +0900

    create `gameRoom` app

--- omok_server/gameRoom/views.py
+++ omok_server/gameRoom/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import GameRoom
+
+def gameRoom(request):
+    return render(
+        request,
+        'gameRoom/index.html'
+    )
--- omok_server/omok_server/urls.py
+++ omok_server/omok_server/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include("home.urls")),
     path('lobby/', include("lobby.urls")),
-    path('admin/', admin.site.urls),
+    path('gameRoom/', include("gameRoom.urls")),
 ]
