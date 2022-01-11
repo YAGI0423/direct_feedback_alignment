061483d837fb9fafb079399aaaccd9b313ca5330
YAGI0423 <9945735@naver.com>
2022-01-11 12:08:53 +0900

    update `lobby/urls.py`

--- omok_server/lobby/urls.py
+++ omok_server/lobby/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.gameRoom),   #int값을 pk변수로 반환
     path('', views.roomList),
+    path('<int:pk>/', views.gameRoom),   #int값을 pk변수로 반환
 ]
