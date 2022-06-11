34da21303ddbfb8adf7554fd1532c2aac7a130a8
YAGI0423 <9945735@naver.com>
2022-06-11 16:57:45 +0900

    create(during) `webController`

---
+++ webController.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+
+
+class WebController:
+    def __init__(self):
+        def init_option(option):
+            option.add_argument("disable-gpu")   # 가속 사용 x
+            option.add_argument('lang=ko_KR')   #플러그인 탑재
+            option.add_argument(
+                'user-agent=Mozilla/5.0 (Macintosh; \
+                Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) \
+                Chrome/61.0.3163.100 Safari/537.36'
+            )
+
+        
+        self.web_option = webdriver.ChromeOptions()
+        init_option(option=self.web_option)
+
+        self.driver = webdriver.Chrome('./chromedriver/chromedriver')
+        
+    
+    def open_browser(self, url):
+        self.driver.maximize_window()    #창 크기 최대화
+        self.driver.get(url)    #url 접속
+
+
+if __name__ == '__main__':
+    web_ctrl = WebController()
+
+    web_ctrl.open_browser('https://www.google.com/')
